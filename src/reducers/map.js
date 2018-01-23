7e2a0257f6faf3c65d2af48655cb79224c145b7c
Xdudu <liuyuewuyu@yeah.net>
2018-01-23 15:08:32 +0800

    Add: addParent and addChild

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -12,22 +12,22 @@ const initialState = {
     '0': rootItem
 }
 
-const genNewItem = parentId => ({
+const genNewItem = (parentId, childIds) => ({
     id: genUid(),
     text: '',
-    childIds: [],
+    childIds: childIds || [],
     parentId
 })
 
 const addSiblings = (state, id, ifAddBefore) => {
     const commonParentId = state[id].parentId,
         commonParent = state[commonParentId],
-        idIndex = commonParent.childIds.indexOf(id) + (ifAddBefore ? 0 : 1),
+        newItemIdIndex = commonParent.childIds.indexOf(id) + (ifAddBefore ? 0 : 1),
         updatedChildIdsOfParent = [...commonParent.childIds];
         
     const newItem = genNewItem(commonParentId);
     
-    updatedChildIdsOfParent.splice(idIndex, 0, newItem.id);
+    updatedChildIdsOfParent.splice(newItemIdIndex, 0, newItem.id);
     
     return {
         ...state, 
@@ -36,6 +36,46 @@ const addSiblings = (state, id, ifAddBefore) => {
             childIds: updatedChildIdsOfParent
         },
         [newItem.id]: newItem
+    }
+}
+
+const addParent = (state, id) => {
+    const currParentId = state[id].parentId,
+        currParent = state[currParentId],
+        idIndex = currParent.childIds.indexOf(id),
+        updatedChildIdsOfParent = [...currParent.childIds];
+        
+    const newParentItem = genNewItem(currParentId, [...currParent.childIds]);
+    
+    updatedChildIdsOfParent.splice(idIndex, 1, newParentItem.id);
+    
+    return {
+        ...state, 
+        [currParentId]: {
+            ...currParent,
+            childIds: updatedChildIdsOfParent
+        },
+        [id]: {
+            ...state[id],
+            parentId: newParentItem.id
+        },
+        [newParentItem.id]: newParentItem
+    }
+}
+
+const addChild = (state, id) => {
+    const updatedChildIds = [...state[id].childIds];
+    const newChildItem = genNewItem(id);
+    
+    updatedChildIds.push(newChildItem.id);
+    
+    return {
+        ...state,
+        [id]: {
+            ...state[id],
+            childIds: updatedChildIds
+        },
+        [newChildItem.id]: newChildItem
     }
 }
 
@@ -48,6 +88,10 @@ const map = (state = initialState, action) => {
             return addSiblings(state, action.id, true)
         case 'ADD_ITEM_AFTER':
             return addSiblings(state, action.id)
+        case 'ADD_PARENT_ITEM':
+            return addParent(state, action.id)
+        case 'ADD_CHILD_ITEM':
+            return addChild(state, action.id)
         default:
             return state
     }
